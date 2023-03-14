9cbee306d4bd8856cfd1bb761e8653d230556dda
canabal97 <martincanabal@hotmail.com>
2023-03-13 22:01:39 -0300

    Actualizado gitignore y corregido ej_1.c

--- ej_1.c
+++ ej_1.c
@@ -1,33 +1,45 @@
 //Calcular el índice de masa corporal ingresando el peso de una persona en kilos y suestatura en metros.
 // Este índice se obtiene realizando el cociente entre el peso y elcuadrado de la estatura.
 
+#include <stdio.h>
+#include <stdlib.h>
 
-void cargar(float peso, float altura){
+float cargar_peso(){
 
-    printf("Ingrese un peso en kgs\n");
+    float peso;
+
+    printf("Ingrese un peso en kgs.:\n");
     scanf("%f", &peso);
 
     while(peso<1 || peso>500){
 
-        printf("El peso no puede se menor a 1kg ni mayor a 500kg. Intentelo nuevamente");
+        printf("El peso no puede se menor a 1kg ni mayor a 500kg. Intentelo nuevamente\n");
         scanf("%f", &peso);
 
     }
 
-    printf("Ingrese una altura en mts\n");
+    return peso;
+}
+
+
+float cargar_altura(){
+
+    float altura;
+
+    printf("Ingrese una altura en metros (dos decimales):\n");
     scanf("%f", &altura);
 
-    while(altura<1 || altura){
+    while(altura<1 || altura>3){
 
-        printf("La altura ingresada es incorrecta. No puede ser menor a 1 o mayor a 3");
+        printf("La altura ingresada es incorrecta. No puede ser menor a 1 o mayor a 3 metros\n");
         scanf("%f", &altura);
 
     }
 
+    return altura;
 }
 
-
-int calculo(float peso, float altura){
+float calculo(float peso, float altura){
 
     float imc = peso/(altura*altura);
 
@@ -35,14 +47,13 @@ int calculo(float peso, float altura){
 }
 
 
-float main(){
+int main(){
 
-    float peso, altura, imc;
+    float peso = cargar_peso(peso);
+    float altura = cargar_altura(altura);
+    float imc = calculo(peso, altura);
 
-    cargar(peso, altura);
+    printf("El Indice de Masa Corporal es de %.2f\n", imc);
 
-    imc = calculo(peso, altura);
-
-    printf("El indice de masa corporal es de %f", imc);
-
+    return 0;
 }
